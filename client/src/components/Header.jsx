21ddb3e506555a9b4b70e6eb4f375d8d6f4cd502
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2024-07-03 22:47:34 +0530

    heder logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,36 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "./logo.png"; // Replace with your logo file path
 
 export default function Header() {
   return (
     <div className='bg-slate-200'>
-        <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-            <Link to='/'>
-            <h1 className='font-bold'>Destiny Library</h1>
-            </Link>
-            <ul className='flex gap-4'>
-                <Link to = '/'>
-                    <li>Home</li>
-                </Link>
-                <Link to='/about'>
-                    <li>About Us</li>
-                </Link>
-                <Link to='/membership'>
-                    <li>Memberships</li>
-                </Link>
-                <Link to='/contact'>
-                    <li>Contact Us</li>
-                </Link>
-                <Link to='/sign-in'>
-                    <li>Sign In</li>
-                </Link>
-                
-                
-                
-                
-               
-            </ul>
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <div className="flex items-center">
+          <img src={logo} alt="Destiny Library Logo" className="h-10 mr-2" /> {/* Adjust height and margin as needed */}
+          <Link to='/'>
+            <h1 className='font-bold text-xl'>Destiny Library</h1>
+          </Link>
         </div>
-
+        <ul className='flex gap-4'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About Us</Link>
+          </li>
+          <li>
+            <Link to='/membership'>Memberships</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact Us</Link>
+          </li>
+          <li>
+            <Link to='/sign-in'>Sign In</Link>
+          </li>     
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
