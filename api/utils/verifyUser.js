0e9c09afe22d84540081406c4b5b4db585e7faab
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2024-09-21 16:06:39 +0530

    add delete and signout functions

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,15 +1,15 @@
 import jwt from 'jsonwebtoken';
-import { errorHandelr } from './error.js';
+import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token; // Using cookies to store token
 
   if (!token) {
-    return next(errorHandelr(401, 'You are not authenticated!'));
+    return next(errorHandler(401, 'You are not authenticated!'));
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return next(errorHandelr(403, 'Invalid token!'));
+    if (err) return next(errorHandler(403, 'Invalid token!'));
     req.user = user;
     next(); // Continue to the next middleware or route handler
   });
